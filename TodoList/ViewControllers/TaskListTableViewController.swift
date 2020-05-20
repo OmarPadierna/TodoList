4043d604b055ddcf7d907d8bb6895465630b064d
Omar Padierna <omar.ps16@gmail.com>
2020-05-19 22:02:12 -0400

    Adds filtering capability to TaskListViewController

--- TodoList/ViewControllers/TaskListTableViewController.swift
+++ TodoList/ViewControllers/TaskListTableViewController.swift
@@ -12,10 +12,10 @@ class TaskListTableViewController: UITableViewController {
     private let searchController = UISearchController(searchResultsController: nil)
 
     var tasks: [Task] = []
+    var unfilteredTasks: [Task] = []
 
     private var selectedTask: Task?
-
-    private var filteredTasks: [Task] = []
+    private var searchResultTasks: [Task] = []
     private var expandedRows: [Bool]  = []
     private var isSearchBarEmpty: Bool {
       return searchController.searchBar.text?.isEmpty ?? true
@@ -34,10 +34,10 @@ class TaskListTableViewController: UITableViewController {
         definesPresentationContext                            = true
 
         //Initialize expandedRows array. Used to keep track of expanded/collapsed state in row
-        expandedRows = tasks.map({ (_) -> Bool in
-            return false
-        })
+        initializeExpandedRows()
 
+        unfilteredTasks = tasks
+        tableView.tableHeaderView?.isHidden = true
     }
 
     // MARK: - Table view data source
@@ -48,7 +48,7 @@ class TaskListTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if isFiltering {
-            return filteredTasks.count
+            return searchResultTasks.count
         }
 
         return tasks.count
@@ -61,7 +61,7 @@ class TaskListTableViewController: UITableViewController {
         let task: Task
 
         if isFiltering {
-            task = filteredTasks[indexPath.row]
+            task = searchResultTasks[indexPath.row]
         } else {
             task = tasks[indexPath.row]
         }
@@ -78,12 +78,12 @@ class TaskListTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
 
         if isFiltering {
-            selectedTask = filteredTasks[indexPath.row]
+            selectedTask = searchResultTasks[indexPath.row]
         } else {
             selectedTask = tasks[indexPath.row]
         }
 
-        let expandedState = expandedRows[indexPath.row]
+        let expandedState           = expandedRows[indexPath.row]
         expandedRows[indexPath.row] = !expandedState
 
         tableView.reloadRows(at: [indexPath], with: .automatic)
@@ -94,26 +94,78 @@ class TaskListTableViewController: UITableViewController {
             tasks.remove(at: indexPath.row)
             expandedRows.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            updateUnfilteredTasks()
         }
     }
 
     // MARK: - Functions
 
     func filterContentForSearchText(_ searchText: String) {
-        filteredTasks = tasks.filter { (task: Task) -> Bool in
+        searchResultTasks = tasks.filter { (task: Task) -> Bool in
         return task.title.lowercased().contains(searchText.lowercased())
       }
 
       tableView.reloadData()
     }
 
+    func updateUnfilteredTasks() {
+        unfilteredTasks = tasks
+    }
+
+    func initializeExpandedRows() {
+        expandedRows = tasks.map({ (_) -> Bool in
+            return false
+        })
+    }
+
+    func clearFilters() {
+        tasks = unfilteredTasks
+
+        initializeExpandedRows()
+
+        tableView.tableHeaderView?.isHidden = true
+        tableView.reloadData()
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch (segue.destination, segue.identifier) {
         case (let vc as AddTaskViewController, _):
             vc.delegate = self
+        case (let vc as CalendarViewController, _):
+            vc.delegate = self
         default:
             print("Unknown segue")
         }
+    }
+
+    private func filterByStatus(_ status: Status) {
+        tasks = tasks.filter { (task) -> Bool in
+            return task.status == status
+        }
+
+        initializeExpandedRows()
+
+        tableView.tableHeaderView?.isHidden = false
+
+        tableView.reloadData()
+    }
+
+    private func filterByDate(_ date: Date) {
+        tasks = tasks.filter({ (task) -> Bool in
+            task.dueDate == date
+        })
+
+        initializeExpandedRows()
+
+        let formatter = DateFormatter()
+        formatter.timeStyle = .none
+        formatter.dateStyle = .medium
+
+        self.title = formatter.string(from: date)
+
+        tableView.tableHeaderView?.isHidden = false
+
+        tableView.reloadData()
     }
 
     // MARK: - IBActions
@@ -122,6 +174,31 @@ class TaskListTableViewController: UITableViewController {
         performSegue(withIdentifier: "addTaskSegue", sender: nil)
     }
 
+    @IBAction func presentOptions(_ sender: Any) {
+        let optionMenu = UIAlertController(title: nil, message: "Filter tasks by", preferredStyle: .actionSheet)
+
+        let filterByDateAction = UIAlertAction(title: "Due date", style: .default) { [unowned self] (_) in
+            self.performSegue(withIdentifier: "calendarSegue", sender: nil)
+        }
+
+        let filterByComplete = UIAlertAction(title: "Complete tasks", style: .default) { [unowned self] (_) in
+            self.filterByStatus(.done)
+        }
+
+        let filterByPending = UIAlertAction(title: "Pending tasks", style: .default) { [unowned self] (_) in
+            self.filterByStatus(.pending)
+        }
+
+        optionMenu.addAction(filterByDateAction)
+        optionMenu.addAction(filterByComplete)
+        optionMenu.addAction(filterByPending)
+
+        self.present(optionMenu, animated: true, completion: nil)
+    }
+
+    @IBAction func clearFiltersButtonTapped(_ sender: Any) {
+        clearFilters()
+    }
 }
 
 extension TaskListTableViewController: UISearchResultsUpdating {
@@ -136,8 +213,14 @@ extension TaskListTableViewController: AddTaskViewControllerDelegate {
 
         tasks.append(task)
         expandedRows.append(false)
-
+        updateUnfilteredTasks()
+        clearFilters()
         tableView.reloadData()
     }
+}
 
+extension TaskListTableViewController: CalendarViewControllerDelegate {
+    func calendarViewController(didFinishWith date: Date) {
+        filterByDate(date)
+    }
 }
